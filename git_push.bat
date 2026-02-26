c05feb914ef4b8ec55f90693f1343c317128c8a3
10wang-creat <s881705@gmail.com>
2026-02-27 05:29:00 +0800

    新增方劑故事

--- git_push.bat
+++ git_push.bat
@@ -1,9 +1,7 @@
 @echo off
 cd /d C:\Users\s8817\OneDrive\Documents\GitHub\herbal-stories-v2
-git add _stories/jia-wei-xiao-yao-san.md
-git add assets/images/characters/mudanpi.png
-git add assets/images/characters/zhizi.png
-git commit -m "Add-Jia-Wei-Xiao-Yao-San-article"
+git add assets/images/characters/fangfeng.png
+git commit -m "Add fangfeng character image"
 git push origin main
 echo Done!
 pause
