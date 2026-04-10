80a9c4f82cf14f9482b04f576947f38711212d44
10wang-creat <s881705@gmail.com>
2026-04-10 22:55:12 +0800

    整理檔案

--- git_push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d C:\Users\s8817\OneDrive\Documents\GitHub\herbal-stories-v2
-git add assets/images/characters/fangfeng.png
-git commit -m "Add fangfeng character image"
-git push origin main
-echo Done!
-pause
